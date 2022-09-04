395f460f81bdc4d926dd8358df7f0126dec23f65
tomast1337 <vycasnicolas@gmail.com>
2022-09-04 01:28:25 -0300

    Adicionado suporte para CORS por motivos de teste

--- src/main/java/com/br/petshop/Controller/AnimalController.java
+++ src/main/java/com/br/petshop/Controller/AnimalController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.br.petshop.Service.IAnimalService;
 
 @RestController
 @RequestMapping(path="/petshop")
+@CrossOrigin(origins = "*")
 public class AnimalController {
 
 	@Autowired
