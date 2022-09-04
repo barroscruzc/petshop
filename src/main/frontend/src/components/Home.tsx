a7f7515ebbd0ad1b5bcf2d8bdbc9d10f4fe23c81
tomast1337 <vycasnicolas@gmail.com>
2022-09-04 01:53:48 -0300

    Pagina home

--- src/main/frontend/src/components/Home.tsx
+++ src/main/frontend/src/components/Home.tsx
@@ -1,7 +1,25 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
+import style from "./Page.module.scss";
 
-export default () => (
-    <>
-        <h1>Home</h1>
-    </>
-);
+export default () => {
+
+
+    return (
+        <div className={style.container}>
+            <h2>Sobre</h2>
+            <p>
+                PetShop é um sistema de gerenciamento de petshops.
+            </p>
+            <p>
+                Para gerenciar os Clientes clique <Link to="/owners">aqui</Link>.
+            </p>
+            <p>
+                Para gerenciar os Pets clique <Link to="/pets">aqui</Link>.
+            </p>
+            <p>
+                Para gerenciar os Atendimentos clique <Link to="/services">aqui</Link>.
+            </p>
+        </div>
+    );
+}
