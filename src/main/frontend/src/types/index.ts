29871ad9d7a5c07c7aae5d2c6e8c79f662bf649b
tomast1337 <vycasnicolas@gmail.com>
2022-09-03 21:37:23 -0300

    Criação de tipos de negocio

---
+++ src/main/frontend/src/types/index.ts
@@ -0,0 +1,31 @@
+
+type Animal = {
+    nome: string;
+    idade: number;
+    peso: number;
+    cliente_id: number;
+}
+
+type Cliente = {
+    id: number;
+    nome: string;
+    telefone: string;
+    animais?: Animal[];
+};
+
+type OrdemServico = {
+    codigo: number;
+    id_cliente: number;
+    id_animal: number;
+    valor: number;
+    data: string;
+    descricao: string;
+    hora_entrada: string;
+    hora_retirada: string;
+};
+
+export {
+    Animal,
+    Cliente,
+    OrdemServico
+};
