dabddb91a494f368397f1a483717fbdf48fee567
tomast1337 <vycasnicolas@gmail.com>
2022-09-08 03:22:21 -0300

    Corrigindo erro de 404

--- src/main/frontend/src/index.tsx
+++ src/main/frontend/src/index.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client';
 import App from "./App";
 import "./index.scss";
 const root = createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 );
